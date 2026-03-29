f3b39d1cb7f74b00aab13c146403ffda39f20d5e
guizixx <guilherme.alcochete@gmail.com>
2026-03-29 23:24:00 +0100

    fix: correção nos métodos accept, envia e recebe.

--- MarketPlace/servidor/skeleton.py
+++ MarketPlace/servidor/skeleton.py
@@ -20,27 +20,31 @@ class Skeleton:
         return self.rede
 
     def accept(self): 
-        self.obter_rede().accept()
-        print("SERVIDOR> Servidor ligado a %s no porto %s" % (self.obter_rede().ponto_acesso.endereco_ip, self.obter_rede().ponto_acesso.port))
+        conn_sock, addr = self.obter_rede().accept()
+        print("SERVIDOR> Servidor ligado a %s no porto %s" % (self.obter_rede().ponto_acesso.endereco_ip, self.obter_rede().ponto_acesso.porto))
+        return conn_sock, addr
 
-    def envia(self, msg_str): 
+    def envia(self, conn_sock, msg_str): 
         try:
-            size = struct.pack('i', len(msg_str))
             bytes = pickle.dumps(msg_str, protocol=pickle.HIGHEST_PROTOCOL)            
-        except excepcoes_shared.ExcecaoSerializacaoInvalida as e:
-            raise e
+            tamanho = struct.pack('!I', len(bytes))
+        except Exception:
+            raise excepcoes_shared.ExcecaoSerializacaoInvalida()
         try:
-            self.obter_rede().envia(size, bytes)
+            self.obter_rede().envia(conn_sock, tamanho, bytes)
         except excepcoes_shared.ExcecaoLigacaoInterrompida as e:
             raise e
         print("Estou a enviar", msg_str)
 
-    def recebe(self): 
-        msg_bytes = self.obter_rede().recebe()
+    def recebe(self, conn_sock): 
+        try:
+            msg_bytes = self.obter_rede().recebe(conn_sock)
+        except excepcoes_shared.ExcecaoLigacaoInterrompida as e:
+            raise e
         try:
             msg = pickle.loads(msg_bytes)
-        except excepcoes_shared.ExcecaoDesserializacaoInvalida as e:
-            raise e
+        except Exception:
+            raise excepcoes_shared.ExcecaoDesserializacaoInvalida()
         print(f"SERVIDOR> Comando recebido: {msg}")
         return msg
 
