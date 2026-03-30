9b94991e48d1cbc788a790ba5626f21e2c51f903
guizixx <guilherme.alcochete@gmail.com>
2026-03-30 23:47:10 +0100

    fix: correção dos nomes dos atributos das classes.

--- MarketPlace/cliente/ClassesReduzidas.py
+++ MarketPlace/cliente/ClassesReduzidas.py
@@ -3,28 +3,28 @@
 class ClienteLoja:
 
     def __init__(self, nome, email, pw, id, permissao):
-        self.id = id
+        self.id_cliente = id
         self.nome = nome
         self.email = email
-        self.pw = pw
+        self.password = pw
         self.carrinho_compras = {}  # { id_produto : quantidade }
         self.permissao = permissao
 
 class Encomenda:
     _contador_global = 1
     def __init__(self, data, carrinho_compras, cliente_id, total):
-        self.id = Encomenda._contador_global
+        self.id_encomenda = Encomenda._contador_global
         self.data = data
-        self.carrinho_compras = carrinho_compras
-        self.cliente_id = cliente_id
-        self.total = total
+        self.produtos = carrinho_compras
+        self.id_cliente = cliente_id
+        self.total_preco = total
         Encomenda._contador_global += 1
 
 class Produto:
     _contador_global = 1
 
     def __init__(self, nome, categoria, preco, quantidade):
-        self.id = Produto._contador_global
+        self.id_produto = Produto._contador_global
         self.nome = nome
         self.categoria = categoria
         self.preco = round(preco, 2)
@@ -35,6 +35,6 @@ class Categoria:
     _contador_global = 1
 
     def __init__(self, nome):
-        self.id = Categoria._contador_global
+        self.id_categoria = Categoria._contador_global
         self.nome = nome
         Categoria._contador_global += 1
